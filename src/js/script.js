b44dbf7a6a5955cb78f29197ddff385eaa5138a2
EloraP <eloraira@gmail.com>
2023-08-04 19:57:06 +0100

    setting up and messing with fonts

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+// Sample JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM loaded. You can now start adding your own JavaScript functionality.');
+});
